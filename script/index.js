64bf31a68205b9ecd7aa2e027e073ee53bd472c9
emonsimply <emonsimply@gmail.com>
2025-08-27 21:00:49 +0600

    Call Button Functionalities added

--- script/index.js
+++ script/index.js
@@ -3,12 +3,49 @@ let coinCount = Number(document.getElementById("coin-count").innerText);
 let copyCount = Number(document.getElementById("copy-count").innerText);
 
 const cards = document.getElementsByClassName("card");
+const historyCard = document.getElementById("history-card");
 
-// Heart Count
 for (let card of cards) {
+  // Heart Count
   const heartBtn = card.childNodes[1].childNodes[3];
   heartBtn.addEventListener("click", function () {
     heartCount++;
     document.getElementById("heart-count").innerText = heartCount;
+  });
+
+  // Call Button
+  const title = card.childNodes[5].innerText;
+  const number = card.childNodes[9].innerText;
+  const now = new Date();
+  const time = now.toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  });
+  console.log(title, number, time);
+
+  const callBtn = card.childNodes[15].childNodes[3];
+  callBtn.addEventListener("click", function () {
+    if (coinCount < 20) {
+      alert("Not enough coins to make a call!");
+      return;
+    }
+
+    alert(`📞 Calling ${title} ${number}`);
+
+    coinCount = coinCount - 20;
+    document.getElementById("coin-count").innerText = coinCount;
+
+    const newHistory = document.createElement("div");
+    newHistory.innerHTML = `
+    <div class="flex justify-between items-center bg-gray-100 p-3 rounded-lg mb-2">
+            <div>
+              <h3 class="font-semibold text-sm">${title}</h3>
+              <p class="text-gray-500">${number}</p>
+            </div>
+            <div class="text-sm">${time}</div>
+          </div>
+    `;
+    historyCard.appendChild(newHistory);
   });
 }
